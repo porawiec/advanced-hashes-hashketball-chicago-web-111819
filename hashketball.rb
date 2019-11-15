24379ee6c151edc9404b93aeb4a0cd1ec86a2b7d
Paul O <paul.orawiec@gmail.com>
2019-11-15 09:25:34 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,11 +1,224 @@
-# Write your code here!
+def game_hash
+  
+  nested_game_hash ={
+    :home => {
+      :team_name => "Brooklyn Nets", 
+      :colors => ["Black", "White"], 
+      :players => [ {
+        :player_name => "Alan Anderson", 
+        :number => 0, 
+        :shoe => 16, 
+        :points => 22, 
+        :rebounds => 12, 
+        :assists => 12, 
+        :steals => 3, 
+        :blocks => 1, 
+        :slam_dunks => 1}, 
+        {
+        :player_name => "Reggie Evans", 
+        :number => 30, 
+        :shoe => 14, 
+        :points => 12, 
+        :rebounds => 12, 
+        :assists => 12, 
+        :steals => 12, 
+        :blocks => 12, 
+        :slam_dunks => 7},
+        {
+        :player_name => "Brook Lopez", 
+        :number => 11, 
+        :shoe => 17, 
+        :points => 17, 
+        :rebounds => 19, 
+        :assists => 10, 
+        :steals => 3, 
+        :blocks => 1, 
+        :slam_dunks => 15},
+        {
+        :player_name => "Mason Plumlee", 
+        :number => 1, 
+        :shoe => 19, 
+        :points => 26, 
+        :rebounds => 11, 
+        :assists => 6, 
+        :steals => 3, 
+        :blocks => 8, 
+        :slam_dunks => 5},
+        {
+        :player_name => "Jason Terry", 
+        :number => 31, 
+        :shoe => 15, 
+        :points => 19, 
+        :rebounds => 2, 
+        :assists => 2, 
+        :steals => 4, 
+        :blocks => 11, 
+        :slam_dunks => 1} 
+      ]
+    },
+   :away => {
+     :team_name => "Charlotte Hornets", 
+      :colors => ["Turquoise", "Purple"], 
+      :players => [ {
+        :player_name => "Jeff Adrien", 
+        :number => 4, 
+        :shoe => 18, 
+        :points => 10, 
+        :rebounds => 1, 
+        :assists => 1, 
+        :steals => 2, 
+        :blocks => 7, 
+        :slam_dunks => 2}, 
+        {
+        :player_name => "Bismack Biyombo", 
+        :number => 0, 
+        :shoe => 16, 
+        :points => 12, 
+        :rebounds => 4, 
+        :assists => 7, 
+        :steals => 22, 
+        :blocks => 15, 
+        :slam_dunks => 10},
+        {
+        :player_name => "DeSagna Diop", 
+        :number => 2, 
+        :shoe => 14, 
+        :points => 24, 
+        :rebounds => 12, 
+        :assists => 12, 
+        :steals => 4, 
+        :blocks => 5, 
+        :slam_dunks => 5},
+        {
+        :player_name => "Ben Gordon", 
+        :number => 8, 
+        :shoe => 15, 
+        :points => 33, 
+        :rebounds => 3, 
+        :assists => 2, 
+        :steals => 1, 
+        :blocks => 1, 
+        :slam_dunks => 0},
+        {
+        :player_name => "Kemba Walker", 
+        :number => 33, 
+        :shoe => 15, 
+        :points => 6, 
+        :rebounds => 12, 
+        :assists => 12, 
+        :steals => 7, 
+        :blocks => 5, 
+        :slam_dunks => 12} 
+      ]
+    }
+  }
+
+end
 
 
+def num_points_scored(name)
+ # returns the number of points scored for that player
+  game_hash.each do | team, team_hash |
+    team_hash[:players].each do | player, player_hash |
+      if player[:player_name] == name
+        return player[:points]
+      end
+    end
+  end
+end
 
 
+def shoe_size(name)
+ # returns the shoe size for that player.
+  game_hash.each do | team, team_hash |
+    team_hash[:players].each do | player, player_hash |
+      if player[:player_name] == name
+        return player[:shoe]
+      end
+    end
+  end
+end
 
 
+def team_colors(team_name)
+ # returns an Array of that team's colors.
+  game_hash.each do | team, team_hash |
+    if team_hash[:team_name] == team_name
+        return team_hash[:colors]
+    end
+  end
+end
 
 
+def team_names
+ # return an Array of the team names
+  team_array = []
+  game_hash.each do | team, team_hash |
+     team_array.push(team_hash[:team_name])
+  end
+  team_array
+end
 
 
+def player_numbers(team_name)
+ # returns an Array of the jersey numbers for that team
+ jerseys_array = []
+  game_hash.each do | team, team_hash |
+    if team_hash[:team_name] == team_name
+      team_hash[:players].map do | indiv_players, jersey_hash |
+        jerseys_array.push(indiv_players[:number])
+      end
+    end
+  end
+  jerseys_array
+end
+
+
+def player_stats(name)
+  # player_stats("Alan Anderson")
+  #=> { :number => 0,
+  #      :shoe => 16,
+  #      :points => 22,
+  #      :rebounds => 12,
+  #      :assists => 12,
+  #      :steals => 3,
+  #      :blocks => 1,
+  #      :slam_dunks => 1
+  #    }
+  game_hash.each do | team, team_hash |
+    team_hash[:players].each do | player, player_hash |
+      if player[:player_name] == name
+        player.delete(:player_name)
+        return player
+      end
+    end
+  end
+end
+
+
+def big_shoe_rebounds
+ # return the number of rebounds associated with the player that has the largest shoe size
+ 
+end
+
+
+#### BONUS ####
+
+def most_points_scored
+  # returns which player has the most points
+end
+
+
+def winning_team
+  # returns which team has the most points
+end
+
+
+def player_with_longest_name
+  # returns which player has the longest name
+end
+
+
+def long_name_steals_a_ton?
+  # returns true if the player with the longest name had the most steals.
+end
